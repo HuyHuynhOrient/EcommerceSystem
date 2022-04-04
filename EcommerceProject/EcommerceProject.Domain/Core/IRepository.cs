6b605fb1ac63f8720f8a6b4695914fee497bbbd8
Huynh Duc Quang Huy <huy.huynhducquang@orientsoftware.com>
2022-04-04 17:34:31 +0700

    Create again a project api with DDD and CQRS

---
+++ EcommerceProject/EcommerceProject.Domain/Core/IRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceProject.Domain.Core
+{
+    public interface IRepository<TAggregateRoot, in TId> where TAggregateRoot : AggregateRoot<TId>
+    {
+        Task<bool> ExistsAsync(TId id, CancellationToken cancellationToken = default);
+
+        Task<bool> ExistsAsync(ISpecification<TAggregateRoot> specification, CancellationToken cancellationToken = default);
+        
+        Task<TAggregateRoot> FindOneAsync(TId id, CancellationToken cancellationToken = default);
+        
+        Task<TAggregateRoot> FineOneAsync(ISpecification<TAggregateRoot> specification, CancellationToken cancellationToken = default);
+        
+        Task<IEnumerable<TAggregateRoot>> FindAllAsync(ISpecification<TAggregateRoot> specification,
+            CancellationToken cancellationToken = default);
+        
+        Task AddAsync(TAggregateRoot aggregate, CancellationToken cancellationToken = default);
+        
+        Task SaveAsync(TAggregateRoot aggregate, CancellationToken cancellationToken = default);
+        
+        Task DeleteAsync(TAggregateRoot aggregate, CancellationToken cancellationToken = default);
+    }
+}
---
+++ EcommerceProject/EcommerceProject.Domain/Core/ISpecification.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq.Expressions;
+
+namespace EcommerceProject.Domain.Core
+{
+    public interface ISpecification<T>
+    {
+        Expression<Func<T, bool>> Expression { get; }
+        List<Expression<Func<T, object>>> Includes { get; }
+    }
+}
