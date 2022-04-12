2bf2dd6a79e7d57d06e1b219b1a0afe2210f13c3
Huynh Duc Quang Huy <huy.huynhducquang@orientsoftware.com>
2022-04-12 10:08:24 +0700

    Reset

--- EcommerceProject/EcommerceProject.Application/Queries/Products/GetProducts/GetProductsQuery.cs
+++
@@ -1,9 +0,0 @@
-﻿using EcommerceProject.Domain.AggregatesModel.ProductAggregate;
-using EcommerceProject.Infrastructure.CQRS.Queries;
-
-namespace EcommerceProject.Application.Queries.Products.GetProducts
-{
-    public class GetProductsQuery : IQuery<IEnumerable<Product>>
-    {
-    }
-}
--- EcommerceProject/EcommerceProject.Domain/AggregatesModel/CustomerAggregate/ICustomerRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using EcommerceProject.Domain.SeedWork;
-
-namespace EcommerceProject.Domain.AggregatesModel.CustomerAggregate
-{
-    public interface ICustomerRepository : IBaseRepository<Customer, Guid>
-    {
-    }
-}
