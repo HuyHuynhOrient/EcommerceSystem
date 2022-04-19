9f0539087d7a053eb8a04aa101145f9f53350136
Huynh Duc Quang Huy <huy.huynhducquang@orientsoftware.com>
2022-04-19 17:46:48 +0700

    Commit 19042022

---
+++ EcommerceProject/Test/EcommerceProject.Application.Test/Commands/Carts/AddProductToCart/AddProductToCartCommandHandlerTest.cs
@@ -0,0 +1,42 @@
+﻿using EcommerceProject.Application.Commands.Carts.AddProductToCart;
+using EcommerceProject.Domain.AggregatesRoot.CartAggregate;
+using EcommerceProject.Domain.AggregatesRoot.ProductAggregate;
+using EcommerceProject.Domain.SeedWork;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EcommerceProject.Application.Test.Commands.Carts.AddProductToCart
+{
+    public class AddProductToCartCommandHandlerTest
+    {
+        private Mock<ICartRepository> mockCartRepository = new Mock<ICartRepository>();
+        private Mock<IProductRepository> mockProductRepository = new Mock<IProductRepository>();
+        private AddProductToCartCommand command = new AddProductToCartCommand()
+        {
+            ProductId = 1,
+            Quantity = 1,
+            UserId = Guid.NewGuid(),
+        };
+
+        [Fact]
+        public async Task GivenACart_WhenAddingProductToCart_ThenItShouldBeSuccessful()
+        {
+            var cart = new Cart(command.UserId);
+            mockCartRepository.Setup(p => p.FindOneAsync(It.IsAny<SpecificationBase<Cart>>(), default(CancellationToken))).ReturnsAsync(cart);
+            var handler = new AddProductToCartCommandHandler(mockCartRepository.Object, mockProductRepository.Object);
+
+            var result = await handler.Handle(command, default(CancellationToken));
+
+            Assert.True(result.IsSuccess);
+        }
+
+        [Fact]
+        public async Task Gi
+    }
+}
